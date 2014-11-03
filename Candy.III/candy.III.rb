51ca0adbba5bdbacff7e210f7f23ac0501b92107
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2014-11-03 18:24:51 -0400

    Commented the code of Candy III

--- Candy.III/candy.III.rb
+++ Candy.III/candy.III.rb
@@ -1,7 +1,8 @@
 test_case = gets.chomp
+
 sum = 0;
 
-el_gancho = gets
+el_gancho = gets #this expression is used to read a blank line its like a tramp 
 
 test_case.to_i.times do 
   input = gets.chomp
@@ -10,9 +11,9 @@ test_case.to_i.times do
     sum += gets.chomp.to_i
   end
   
-  sum % input.to_i == 0 ? puts("YES") : puts("NO")
+  sum % input.to_i == 0 ? puts("YES") : puts("NO")   #if the sum mod the number of candy is not 0 then is not posible to put the same number of candies in n heaps
   sum = 0
   
-  el_gancho = gets
+  el_gancho = gets # this is the other obligatory place to read a blank line because each test case is separated by a blank line
 
 end
